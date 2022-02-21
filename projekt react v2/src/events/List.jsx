00f8833d5365dad24be748261aa11c7b703901ec
PrzemyslawPoznanski <56115891+PrzemyslawPoznanski@users.noreply.github.com>
2022-02-21 18:58:03 +0100

    Add files via upload

---
+++ projekt react v2/src/events/List.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { accountService } from '@/_services';
+
+function List({ match }) {
+    const { path } = match;
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        accountService.getAllEvents().then(x => setUsers(x));
+    }, []);
+
+    function deleteUser(id) {
+        setUsers(users.map(x => {
+            if (x.id === id) { x.isDeleting = true; }
+            return x;
+        }));
+        accountService.delete(id).then(() => {
+            setUsers(users => users.filter(x => x.id !== id));
+        });
+    }
+
+    return (
+        <div class="container">
+            <h1>Events</h1>
+            <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Add Event</Link>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th style={{ width: '30%' }}>Band name</th>
+                        <th style={{ width: '30%' }}>Contact info</th>
+                        <th style={{ width: '30%' }}>Status</th>
+                        <th style={{ width: '10%' }}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users && users.map(user =>
+                        <tr key={user.id}>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                            <td>{user.role}</td>
+                            <td style={{ whiteSpace: 'nowrap' }}>
+                                <Link to={`${path}/edit/${user.id}`}  className="btn btn-sm btn-primary mr-1">Edit</Link>
+                                <button onClick={() => deleteUser(user.id)} className="btn btn-sm btn-danger" style={{ width: '60px' }} disabled={user.isDeleting}>
+                                    {user.isDeleting 
+                                        ? <span className="spinner-border spinner-border-sm"></span>
+                                        : <span>Delete</span>
+                                    }
+                                </button>
+                            </td>
+                        </tr>
+                    )}
+                    {!users &&
+                        <tr>
+                            <td colSpan="4" className="text-center">
+                                <span className="spinner-border spinner-border-lg align-center"></span>
+                            </td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export { List };
